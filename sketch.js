2e1926da1ee35ab29cab4c5435d04e93a332bbbf
Sektio Nugroho <128130308+sektinugros@users.noreply.github.com>
2024-09-26 23:45:28 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ function setup() {
 }  
 function draw() {
   background (255,0,0);
-  circle (120,100,100,100)  
-  circle (500,100,100,100) 
-  circle (500,400,100,100) 
-  circle (120,400,100,400) 
+  circle (120,100,100)  
+  circle (500,100,100) 
+  circle (500,400,100) 
+  circle (120,400,100) 
 }
 
