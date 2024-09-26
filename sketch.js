9befbeca06220a26e101e6785f283eb5d85d589b
Sektio Nugroho <128130308+sektinugros@users.noreply.github.com>
2024-09-26 22:24:07 +0700

    Upload 1

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(640, 480);
+}  
+function draw() {
+  background (255,0,0);
+  circle (120,100,100,100)  
+  circle (500,100,100,100) 
+  circle (500,400,100,100) 
+  circle (120,400,100,400) 
+}
+
